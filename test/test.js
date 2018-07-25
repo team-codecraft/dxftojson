e5c23d67eb64f840eda647594ad4c3764358aae8
John <comsamo84@gmail.com>
2018-07-25 12:27:25 +0900

    default dimension type 1

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const assert = require('chai').assert;
 
 describe('Test suit', function () {
   it('should be ok', function (done) {
-    dxftojson('./sample/file2.dxf', 'out.json')
+    dxftojson('./sample/sample2.dxf', 'out.json')
     .then(result => {
 	    assert.containsAllKeys(result, ['HEADER', 'TABLES', 'BLOCKS', 'ENTITIES']);
 			done();  
