87dedf19fdade07fc9d0e2fa3ff1ece84412ba44
John <comsamo84@gmail.com>
2018-09-20 17:35:26 +0900

    테스팅

--- test/test.js
+++ test/test.js
@@ -3,10 +3,10 @@ const assert = require('chai').assert;
 
 describe('Test suit', function () {
   it('should be ok', function (done) {
-    dxftojson('./sample/sample2.dxf', 'out.json')
+    dxftojson('./sample/sample4.dxf', 'out.json')
     .then(result => {
 	    assert.containsAllKeys(result, ['HEADER', 'TABLES', 'BLOCKS', 'ENTITIES']);
-			done();  
+			done();
     })
   });
 });
