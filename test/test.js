619db4ed19dfccd90d6f12abd4e7c0701079bc49
John <comsamo84@gmail.com>
2018-10-02 16:21:21 +0900

    add wipeout entity type

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const assert = require('chai').assert;
 
 describe('Test suit', function () {
   it('should be ok', function (done) {
-    dxftojson('./sample/sample4.dxf', 'out.json')
+    dxftojson('./sample/sample5.dxf', 'out.json')
     .then(result => {
 	    assert.containsAllKeys(result, ['HEADER', 'TABLES', 'BLOCKS', 'ENTITIES']);
 			done();
